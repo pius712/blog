3b03dc7da11d0cf34d08743fbe6f5093e20dec1f
김병창 <bckim@zimssa.com>
2021-03-20 21:05:21 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    title: "pius712",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
